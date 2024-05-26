9d40a4f1bab2bfbd7cbfb9ac7013e8bf24267aba
FractalHenry <legendaryfrosty@gmail.com>
2024-05-26 21:12:14 +0300

    added style and fixed board

--- front/src/pages/boards/board/board.jsx
+++ front/src/pages/boards/board/board.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Card from "../../../components/card";
-let board = ({boardID,boardName}) =>{
+let board = ({boardID,boardName="Noname"}) =>{
     return(
         <div>
             <div className="header flex-col fill">{boardName}</div>
-            <div class="Cards">
-                <Card cardheader="card1"></Card>
-                <Card cardheader="card2"></Card>
-                <Card cardheader="card3"></Card>
-                There will be cards
+            <div className="flex-row">
+                <div className="boardinfo">
+                    info block
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sint nobis natus dicta nemo unde. Dolores modi asperiores ad iste vero voluptas distinctio laboriosam soluta, natus, molestias quaerat, odio delectus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sint nobis natus dicta nemo unde. Dolores modi asperiores ad iste vero voluptas distinctio laboriosam soluta, natus, molestias quaerat, odio delectus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sint nobis natus dicta nemo unde. Dolores modi asperiores ad iste vero voluptas distinctio laboriosam soluta, natus, molestias quaerat, odio delectus?
+
+                </div>
+                <div className="cardswrapper">
+                    <Card cardheader="card1"></Card>
+                    <Card cardheader="card2"></Card>
+                    <Card cardheader="card3"></Card>
+                    <Card cardheader="card4"></Card>
+                    <Card cardheader="card5"></Card>
+                </div>
             </div>
         </div>
     )
